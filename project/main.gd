d9e875f1615f8a589e5f0e3bc76d67d6f4be6917
Mattias Myhrman <mattias.myhrman@gmail.com>
2024-05-18 12:21:51 +0200

    add base project with helix editor

---
+++ project/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("testing helix")
+	pass  # Replace with fu;nction body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
