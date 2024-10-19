ddb2d095593c3a8d1b45c4c14b97f3a92bab53c4
Mattias Myhrman <mattias.myhrman@gmail.com>
2024-10-20 00:13:16 +0200

    fixed debug menu

--- project/autoloads/debug.gd
+++ project/autoloads/debug.gd
@@ -9,27 +9,28 @@ enum DEBUG_EVENT_TYPE {EVENT_OPEN_DEBUG_MENU,EVENT_OPEN_GAME_SELECTOR,EVENT_RESE
 #export (String,FILE,"*.tscn") var forced_level = "res://zen/levels/"
 #export (PackedScene) var test_scene = null
 #export (bool) var force_test_scene = false
-@export var use_local_battle_db = false
+@export var use_local_battle_db : bool
 #export (bool) var toggle_orientation = false
 #export (bool) var force_game_selector_at_start = false
-@export var asset_variant = 0
+@export var asset_variant : int
 
 
 
-#onready var popup_debug = $"%popup_debug"
+@export var popup_debug : Control
+@export var v_box_container_buttons : VBoxContainer
 #onready var v_box_container_buttons = $"%v_box_container_buttons"
 #onready var popup_debug_game = $"%popup_debug_game"
 #onready var v_box_container_buttons_game = $"%v_box_container_buttons_game"
 #var _main = null
 
-#func _on_debug_event(event,_data = null):
-	#match event:
-		#DEBUG_EVENT_TYPE.EVENT_OPEN_DEBUG_MENU:
-			#popup_debug.popup_centered()
+func _on_debug_event(event,_data = null):
+	match event:
+		DEBUG_EVENT_TYPE.EVENT_OPEN_DEBUG_MENU:
+			popup_debug.show()
 		#DEBUG_EVENT_TYPE.EVENT_OPEN_GAME_SELECTOR:
 			#popup_debug_game.popup_centered()
-	#pass
-#
+	pass
+
 #func set_force_level(_force):
 	#force_level = _force
 #
@@ -39,11 +40,12 @@ enum DEBUG_EVENT_TYPE {EVENT_OPEN_DEBUG_MENU,EVENT_OPEN_GAME_SELECTOR,EVENT_RESE
 #func get_force_level():
 	#return force_level if OS.is_debug_build() else false
 #
-#func _ready():
-	#print("debug ready")
-	#connect(SIGNAL_DEBUG,self,"_on_debug_event")
-	#for btn in v_box_container_buttons.get_children():
-		#btn.connect("pressed",self,"debug_button_pressed",[btn.name])
+func _ready():
+	print("debug ready")
+	popup_debug.hide()
+	connect(SIGNAL_DEBUG,Callable(self,"_on_debug_event"))
+	for btn in v_box_container_buttons.get_children():
+		btn.connect("pressed",Callable(self,"debug_button_pressed").bind(btn.name))
 	#for btn in v_box_container_buttons_game.get_children():
 		#btn.connect("pressed",self,"debug_game_selector_button_pressed",[btn.name])
 #
@@ -63,39 +65,39 @@ enum DEBUG_EVENT_TYPE {EVENT_OPEN_DEBUG_MENU,EVENT_OPEN_GAME_SELECTOR,EVENT_RESE
 		#_:
 			#print("button selected: ",name)
 #
-#func debug_button_pressed(name):
-	#match name:
-		#"button_close":
-			#popup_debug.hide()
-		#"button_pop_enemy":
-			#print("pop enemy")
-			#for n in 3:
-				#var new_card = card_controller.create_unit_from_id(n,1)
-				#new_card.context = cards.CONTEXT.LINEUP
-				#core.emit_signal("event",core.EVENT_TYPE.ENEMY_LINEUP_ADD_CARD,[new_card,n])
-			#for n in 3:
-				#var new_card = card_controller.create_unit_from_id(n,1)
-				#new_card.context = cards.CONTEXT.LINEUP
-				#core.emit_signal("event",core.EVENT_TYPE.LINEUP_ADD_CARD,[new_card,n])
-		#"select_game":
-			#print("select game")
-			#popup_debug.hide()
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_OPEN_GAME_SELECTOR,null)
-		#"reset_current_match_level":
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_RESET_MATCH_LEVEL,null)
-		#"match_level_1":
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_01"])
-		#"match_level_2":
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_02"])
-		#"match_level_3":
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_03"])
-		#"match_level_4":
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_04"])
-		#"match_level_5":
-			#debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_05"])
-			#pass
-		#_:
-				#print("unnused button pressed: ",name)
+func debug_button_pressed(name):
+	match name:
+		"button_close":
+			popup_debug.hide()
+		"button_pop_enemy":
+			print("pop enemy")
+			for n in 3:
+				var new_card = await card_controller.create_unit_from_id(n,1)
+				new_card.context = cards.CONTEXT.LINEUP
+				core.emit_signal("event",core.EVENT_TYPE.ENEMY_LINEUP_ADD_CARD,[new_card,n])
+			for n in 3:
+				var new_card = await card_controller.create_unit_from_id(n,1)
+				new_card.context = cards.CONTEXT.LINEUP
+				core.emit_signal("event",core.EVENT_TYPE.LINEUP_ADD_CARD,[new_card,n])
+		"select_game":
+			print("select game")
+			popup_debug.hide()
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_OPEN_GAME_SELECTOR,null)
+		"reset_current_match_level":
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_RESET_MATCH_LEVEL,null)
+		"match_level_1":
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_01"])
+		"match_level_2":
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_02"])
+		"match_level_3":
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_03"])
+		"match_level_4":
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_04"])
+		"match_level_5":
+			debug.emit_signal(debug.SIGNAL_DEBUG,debug.DEBUG_EVENT_TYPE.EVENT_FORCE_LOAD_MATCH_LEVEL,["level_05"])
+			pass
+		_:
+				print("unnused button pressed: ",name)
 #
 #func _process(_delta):
 	#if toggle_orientation:
