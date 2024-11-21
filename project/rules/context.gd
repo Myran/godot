e36011059218fc2eee5c4700f1716e836d39bec6
Myran <mattias.myhrman@gmail.com>
2024-11-21 21:40:42 +0100

    removed comments

--- project/rules/context.gd
+++ project/rules/context.gd
@@ -11,16 +11,6 @@ class Event:
 	pass
 
 
-#	var event_type
-#	var solve_type
-#	var data
-
-#	func _init(_solve_type, _event_type, _data) -> void:
-#		event_type = _event_type
-#		solve_type = _solve_type
-#		data = _data
-
-
 func add_event(_event) -> Context:
 	unresolved_events.append(_event)
 	return self
