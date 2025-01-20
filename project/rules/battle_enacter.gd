ef113c9eaa72964eb36c22f8f357ee298b85577b
Myran <mattias.myhrman@gmail.com>
2025-01-20 19:32:05 +0100

    add types enacter

--- project/rules/battle_enacter.gd
+++ project/rules/battle_enacter.gd
@@ -1,27 +1,27 @@
 class_name BattleEnacter extends Node
 
-var battle_layer
-var holder_allies
-var holder_enemy
+var battle_layer: Node
+var holder_allies: Node
+var holder_enemy: Node
 
 
-func _init(_battle_layer, _holder_allies, _holder_enemy):
+func _init(_battle_layer: Node, _holder_allies: Node, _holder_enemy: Node) -> void:
 	battle_layer = _battle_layer
 	holder_allies = _holder_allies
 	holder_enemy = _holder_enemy
 
 
-func enact(battle_events):
+func enact(battle_events: Array) -> void:
 	printt("Start animating battle")
 	printt("current battle events", battle_events)
 
-	var back_tween
-	var allies = holder_allies.get_current_lineup(true, battle_layer)
-	var enemies = holder_enemy.get_current_lineup(true, battle_layer)
+	var back_tween: Tween
+	var allies: Dictionary = holder_allies.get_current_lineup(true, battle_layer)
+	var enemies: Dictionary = holder_enemy.get_current_lineup(true, battle_layer)
 	holder_allies.hide_lineup()
 	holder_enemy.hide_lineup()
 
-	for event in battle_events:
+	for event: BattleContext.BaseEvent in battle_events:
 		printt("current event:", event)
 		if event is BattleContext.AddLineupEvent:
 			print("Add lineup")
@@ -31,27 +31,27 @@ func enact(battle_events):
 			print("Select active unit")
 		elif event is BattleContext.CombatEvent:
 			print("Combat")
-			var attacker = (
+			var attacker: Node2D = (
 				allies[event.attacker] if event.allied_attack else enemies[event.attacker]
 			)
-			var defender = (
+			var defender: Node2D = (
 				enemies[event.defender] if event.allied_attack else allies[event.defender]
 			)
 			printt("attacker", attacker)
 			printt("defender", defender)
-			var combat_tween = create_tween()
+			var combat_tween: Tween = create_tween()
 
-			var anim_speed = 0.2
-			var attack_dist = 0.65
-			var defence_dist = 0.25
-			var highlight_size = Vector2(1.25, 1.25)
-			var highlight_speed = 0.2
-			var back_multiplier = 2
-			var attacker_start_scale = attacker.scale
-			var target_start_pos = defender.global_position
-			var battle_pos = attacker.global_position.lerp(target_start_pos, attack_dist)
-			var target_battle_pos = target_start_pos.lerp(battle_pos, defence_dist)
-			var start_pos = attacker.global_position
+			var anim_speed: float = 0.2
+			var attack_dist: float = 0.65
+			var defence_dist: float = 0.25
+			var highlight_size: Vector2 = Vector2(1.25, 1.25)
+			var highlight_speed: float = 0.2
+			var back_multiplier: int = 2
+			var attacker_start_scale: Vector2 = attacker.scale
+			var target_start_pos: Vector2 = defender.global_position
+			var battle_pos: Vector2 = attacker.global_position.lerp(target_start_pos, attack_dist)
+			var target_battle_pos: Vector2 = target_start_pos.lerp(battle_pos, defence_dist)
+			var start_pos: Vector2 = attacker.global_position
 			(
 				combat_tween
 				. tween_property(defender, "scale", highlight_size, highlight_speed)
@@ -116,11 +116,11 @@ func enact(battle_events):
 
 		elif event is BattleContext.DamageEvent:
 			print("Damage", event)
-			var u = allies[event.target] if event.side else enemies[event.target]
+			var u: Node2D = allies[event.target] if event.side else enemies[event.target]
 			printt("Damage target:", u, "Damage", event.damage_amount)
 		elif event is BattleContext.ShieldEvent:
 			print("Shield")
-			var u = allies[event.target] if event.side else enemies[event.target]
+			var u: Node2D = allies[event.target] if event.side else enemies[event.target]
 			if event.new_shield_state:
 				u.show_shield()
 			else:
@@ -129,20 +129,20 @@ func enact(battle_events):
 
 		elif event is BattleContext.StatChangeEvent:
 			print("Stat Change", event)
-			var unit_side = allies if event.side else enemies
+			var unit_side: Dictionary = allies if event.side else enemies
 			if !unit_side.has(event.target):
 				continue
 			if event.new_stat == 0:
 				continue
-			var u = unit_side[event.target]
+			var u: Node2D = unit_side[event.target]
 			match event.stat:
 				Battle.UNIT_HEALTH:
 					u.base.set_card_health(event.new_stat)
 
 		elif event is BattleContext.DeathEvent:
 			print("Death")
-			var side = allies if event.side else enemies
-			var dying_u = side[event.pos]
+			var side: Dictionary = allies if event.side else enemies
+			var dying_u: Node2D = side[event.pos]
 			side.erase(event.pos)
 			dying_u.shake(event.side)
 
@@ -155,6 +155,6 @@ func enact(battle_events):
 			await back_tween.finished
 
 	await get_tree().create_timer(1.25).timeout
-	for side in [allies, enemies]:
-		for k in side.keys():
+	for side: Dictionary in [allies, enemies]:
+		for k: Array in side.keys():
 			side[k].queue_free()
