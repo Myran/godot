a8d2955218616d8afce52e7167efa32757f50958
Myran <mattias.myhrman@gmail.com>
2025-02-17 20:52:12 +0100

    fixed usage of const in for health

--- project/rules/ability_health_on_death.gd
+++ project/rules/ability_health_on_death.gd
@@ -3,15 +3,20 @@ class_name DeathTriggerHealthAbility extends Ability
 
 var health_bonus: int
 
+
 func _init(bonus_health: int = 1) -> void:
 	health_bonus = bonus_health
 
-func handle_battle_event(phase: core.Tempus, unit_position: int, is_allied_unit: bool, battle_context: BattleContext, battle_event: Context.Event) -> void:
+
+func handle_battle_event(
+	phase: core.Tempus,
+	unit_position: int,
+	is_allied_unit: bool,
+	battle_context: BattleContext,
+	battle_event: Context.Event
+) -> void:
 	if phase == core.Tempus.POST and battle_event is BattleContext.DeathEvent:
 		var stat_event: BattleContext.StatChangeEvent = BattleContext.StatChangeEvent.new(
-			"current_health",
-			unit_position,
-			is_allied_unit,
-			health_bonus
+			Battle.UNIT_HEALTH, unit_position, is_allied_unit, health_bonus
 		)
 		battle_context.add_event(stat_event)
