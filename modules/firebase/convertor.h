630fb8f25ae96ecafcb3e88342529dc30ef86496
Myran <mattias.myhrman@gmail.com>
2025-05-07 08:54:04 +0200

    start of firebase refactoring

--- modules/firebase/convertor.h
+++ modules/firebase/convertor.h
@@ -2,16 +2,20 @@
 #define FirebaseConvertor_h
 
 #include "core/object/ref_counted.h"
-#include "functions.h"
-#include "firebase.h"
+#include "core/variant/variant.h"
+#include "core/string/ustring.h"
+#include "core/variant/dictionary.h"
 #include "firebase/variant.h"
+#include <vector>
+#include <map>
 
 class Convertor {
-    public:
-    static Variant fromFirebaseVariant(const firebase::Variant& arg);
-    static firebase::Variant toFirebaseVariant(const Dictionary& arg);
-    static firebase::Variant toFirebaseVariant(const String& arg);
-
+public:
+	static Variant fromFirebaseVariant(const firebase::Variant& arg);
+	static firebase::Variant toFirebaseVariant(const String& arg);
+	static firebase::Variant toFirebaseVariant(const Dictionary& arg);
+	// Declaration for the general Variant converter
+	static firebase::Variant toFirebaseVariant(const Variant& arg);
 };
 
 #endif // FirebaseConvertor_h
--- modules/firebase/database.h
+++ modules/firebase/database.h
@@ -2,63 +2,109 @@
 #define FirebaseDatabase_h
 
 #include "core/object/ref_counted.h"
-#include "firebase/database.h"
+#include "core/variant/variant.h"
+#include "core/string/ustring.h"
+#include "core/os/os.h"
+#include "core/templates/vector.h"
+
+// Forward declare Firebase SDK types where possible
+namespace firebase {
+	class App;
+	namespace database {
+		class Database;
+		class DatabaseReference;
+		class Query;
+		class ChildListener;
+		class ValueListener;
+		class DataSnapshot; // Correct: Use class
+		class MutableData;
+		// Removed enum forward decls
+		struct ServerTimestamp {}; // Keep placeholder struct
+	}
+	template <typename ResultType>
+	class Future;
+}
+
 #include "firebase.h"
+#include <map>
+
+// Include REQUIRED Firebase SDK headers
+#include "firebase/database.h"
+#include "firebase/future.h"
+#include "firebase/database/common.h" // Defines Error enum
+#include "firebase/database/listener.h"
+#include "firebase/database/data_snapshot.h"
+#include "firebase/database/transaction.h" // Defines TransactionResult, MutableData
+#include "firebase/database/database_reference.h"
+#include "firebase/database/query.h"
 
 class FirebaseDatabase;
 
+// --- Listener Definitions ---
 class FirebaseChildListener : public firebase::database::ChildListener {
-private:
-    FirebaseDatabase *database;
+	FirebaseDatabase* database_instance_ptr;
 public:
-    FirebaseChildListener(FirebaseDatabase *db);
-    void OnCancelled(const firebase::database::Error & error, const char *error_message);
-    void OnChildAdded(const firebase::database::DataSnapshot & snapshot, const char *previous_sibling_key);
-    void OnChildChanged(const firebase::database::DataSnapshot & snapshot, const char *previous_sibling_key);
-    void OnChildMoved(const firebase::database::DataSnapshot & snapshot, const char *previous_sibling_key);
-    void OnChildRemoved(const firebase::database::DataSnapshot & snapshot);
+	FirebaseChildListener(FirebaseDatabase* db);
+	virtual ~FirebaseChildListener() {}
+	void OnCancelled(const firebase::database::Error& error_code, const char* error_message) override;
+	void OnChildAdded(const firebase::database::DataSnapshot& snapshot, const char* previous_sibling) override;
+	void OnChildChanged(const firebase::database::DataSnapshot& snapshot, const char* previous_sibling) override;
+	void OnChildMoved(const firebase::database::DataSnapshot& snapshot, const char* previous_sibling) override;
+	void OnChildRemoved(const firebase::database::DataSnapshot& snapshot) override;
 };
 
+class ConnectionStateListener : public firebase::database::ValueListener {
+public: // Made public
+	FirebaseDatabase* database_instance_ptr;
+public:
+	ConnectionStateListener(FirebaseDatabase* db);
+	virtual ~ConnectionStateListener() {}
+	void OnValueChanged(const firebase::database::DataSnapshot& snapshot) override;
+	void OnCancelled(const firebase::database::Error& error_code, const char* error_message) override;
+};
+
+
+// --- Transaction Data ---
+struct TransactionData {
+	int request_id;
+	int increment_by;
+	FirebaseDatabase* database_ptr;
+};
+
+// --- Main Database Class ---
 class FirebaseDatabase : public RefCounted {
-    GDCLASS(FirebaseDatabase, RefCounted);
-    
-    protected:
-    static bool inited;
-    static firebase::database::Database *database;
-    static firebase::database::DatabaseReference dbref;
-    static FirebaseChildListener *listener;
-    static void _bind_methods();
+	GDCLASS(FirebaseDatabase, RefCounted);
 
-    //Variant ConvertVariant(const firebase::Variant& val);
-    firebase::database::DatabaseReference GetReferenceToPath(const Array& keys);
-    firebase::database::Query GetQueryFromReference(const firebase::database::DatabaseReference& ref, const Dictionary& query_params);
+protected:
+	static bool is_initialized;
+	static firebase::database::Database *database_instance;
+	static FirebaseChildListener *child_listener_instance;
+	static ConnectionStateListener *connection_listener_instance; // Static pointer declaration
+	static void _bind_methods();
 
-    public:
-    
-    FirebaseDatabase();
-    ~FirebaseDatabase();
-    void SetDBRoot(const Array& keys);
-    void SetValue(const Array& keys, const Variant& value);
-    String PushChild(const Array& keys);
-    void UpdateChildren(const Array& paths, const Dictionary& params);
-    void RemoveValue(const Array& keys);
-    void GetValue(const Array& keys);
-    void OnGetValue(const firebase::Future<firebase::database::DataSnapshot>& result, void* user_data);
-    
-    // New query functionality
-    void QueryOrderedData(const Array& keys, const Dictionary& query_params);
-    void OnQueryResult(const firebase::Future<firebase::database::DataSnapshot>& result, void* user_data);
-    
-    // Server timestamp
-    void SetServerTimestamp(const Array& keys);
-    
-    // Transaction support - simplified to numeric increment
-    void RunTransaction(const Array& keys, int increment_by = 1);
-    void OnTransactionCompleted(const firebase::Future<firebase::database::DataSnapshot>& result, void* user_data);
-    
-    // Connection state monitoring
-    void MonitorConnectionState();
-    void OnConnectionStateChanged(const firebase::database::DataSnapshot& snapshot);
+	firebase::database::DatabaseReference get_reference_to_path(const Array& keys);
+	firebase::database::Query get_query_from_reference(const firebase::database::DatabaseReference& ref, const Dictionary& query_params);
+
+	static firebase::database::TransactionResult increment_transaction_function(
+		firebase::database::MutableData* data, void* transaction_data);
+	static void transaction_completion_callback(
+		const firebase::Future<firebase::database::DataSnapshot>& result, void* transaction_data);
+
+public:
+	FirebaseDatabase();
+	~FirebaseDatabase();
+
+	void get_value_async(int p_request_id, const Array& keys);
+	void set_value_async(int p_request_id, const Array& keys, const Variant& value);
+	void push_and_update_async(int p_request_id, const Array& keys, const Dictionary& data);
+	void remove_value_async(int p_request_id, const Array& keys);
+	void query_ordered_data_async(int p_request_id, const Array& keys, const Dictionary& query_params);
+	void run_transaction_async(int p_request_id, const Array& keys, int increment_by);
+	void set_server_timestamp_async(int p_request_id, const Array& keys);
+	void add_listener_at_path(const Array& keys);
+	void remove_listener_at_path(const Array& keys);
+	void monitor_connection_state();
+	void on_connection_state_changed(const firebase::database::DataSnapshot& snapshot);
 };
 
 #endif // FirebaseDatabase_h
