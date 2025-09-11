211b424a64f22a5a80d9742fe14bda3444a3ef40
Myran <mattias.myhrman@gmail.com>
2025-09-11 20:11:30 +0200

    fix signalawaiter issue

--- project/misc/signal_awaiter.gd
+++ project/misc/signal_awaiter.gd
@@ -3,23 +3,23 @@ class_name SignalAwaiter extends Node
 signal finished
 
 
-func _init() -> void:  # Added return type
+func _init() -> void:
 	var win: Window = Engine.get_main_loop().root
 	win.call_deferred("add_child", self)
 
 
-func add(_signal: Signal) -> SignalAwaiter:  # Added return type
+func add(_signal: Signal) -> SignalAwaiter:
 	@warning_ignore("return_value_discarded")
 	_signal.connect(_on_signal_received.bind(_signal), CONNECT_ONE_SHOT)
 	return self
 
 
-func finish() -> void:  # Added return type
+func finish() -> void:
 	finished.emit()
 	queue_free()
 
 
-func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> void:
+func _on_signal_received(_signal_param = null, _signal = null) -> void:
 	Log.error(
 		"Method not implemented in SignalAwaiter base class",
 		{"method": "_on_signal_received"},
@@ -31,19 +31,19 @@ func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> voi
 class Any:
 	extends SignalAwaiter
 
-	func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> void:
+	func _on_signal_received(_signal_param = null, _signal = null) -> void:
 		finish()
 
 
 class Count:
 	extends SignalAwaiter
-	var _connections: int  # Added type hint
+	var _connections: int
 
-	func _init(count: int) -> void:  # Added param and return type
+	func _init(count: int) -> void:
 		super()
 		_connections = count
 
-	func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> void:
+	func _on_signal_received(_signal_param = null, _signal = null) -> void:
 		if get_incoming_connections().size() == _connections:
 			finish()
 
@@ -51,19 +51,19 @@ class Count:
 class All:
 	extends Count
 
-	func _init() -> void:  # Added return type
+	func _init() -> void:
 		super(0)
 
 
 class SequenceBreak:
 	extends SignalAwaiter
-	var _signals: Array[Signal]  # Added typed array
+	var _signals: Array[Signal]
 
-	func _init(signals: Array[Signal]) -> void:  # Added typed array param
+	func _init(signals: Array[Signal]) -> void:
 		super()
 		_signals = signals
 
-	func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> void:
+	func _on_signal_received(_signal_param = null, _signal = null) -> void:
 		if _signal != _signals[0]:
 			finish()
 
@@ -71,7 +71,7 @@ class SequenceBreak:
 class SequenceMatch:
 	extends SequenceBreak
 
-	func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> void:
+	func _on_signal_received(_signal_param = null, _signal = null) -> void:
 		if _signal == _signals[0]:
 			_signals.remove_at(0)
 			if _signals.is_empty():
@@ -87,6 +87,6 @@ class Timeout:
 		_timer = Engine.get_main_loop().create_timer(timeout_seconds)
 		add(_timer.timeout)
 
-	func _on_signal_received(signal_param = null, _signal: Signal = Signal()) -> void:
+	func _on_signal_received(_signal_param = null, _signal = null) -> void:
 		# Timer expired - finish the awaiter
 		finish()
